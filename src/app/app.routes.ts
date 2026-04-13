07aae9bd362bd19dcaf92407697532338317f6e2
luu-lui <luiggi.guachun@est.ucacue.edu.ec>
2026-04-13 01:07:13 -0500

    Hice cambios en routes y components :3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'contacto', component: ContactoComponent }
+];
